d299a0c4dd2368f7e44c7246dbe0145a7b788616
Marko Gargenta <marko@marakana.com>
2013-07-29 14:42:04 -0700

    Implemented PostTask with ProgressDialog

--- Yamba/src/com/symantec/yamba/StatusActivity.java
+++ Yamba/src/com/symantec/yamba/StatusActivity.java
@@ -1,6 +1,8 @@
 package com.symantec.yamba;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -10,58 +12,87 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.marakana.android.yamba.clientlib.YambaClient;
 import com.marakana.android.yamba.clientlib.YambaClientException;
 
-public class StatusActivity extends Activity implements OnClickListener {
+public class StatusActivity extends Activity {
 	private static final String TAG = "StatusActivity";
 	private Button buttonTweet;
 	private EditText editStatus;
 	private TextView textCount;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_status);
-        
-        buttonTweet = (Button) findViewById(R.id.button_tweet);
-        editStatus = (EditText) findViewById(R.id.edit_status);
-        textCount = (TextView) findViewById(R.id.text_count);
-        
-        buttonTweet.setOnClickListener( this );
-        
-        editStatus.addTextChangedListener( new TextWatcher() {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_status);
+
+		buttonTweet = (Button) findViewById(R.id.button_tweet);
+		editStatus = (EditText) findViewById(R.id.edit_status);
+		textCount = (TextView) findViewById(R.id.text_count);
+		
+		editStatus.addTextChangedListener(new TextWatcher() {
 			@Override
 			public void afterTextChanged(Editable s) {
 				int count = 140 - editStatus.length();
-				textCount.setText( Integer.toString(count) );
+				textCount.setText(Integer.toString(count));
 			}
+			
 			@Override
 			public void beforeTextChanged(CharSequence s, int start, int count,
 					int after) {
 			}
+			
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before,
 					int count) {
-			}        	
-        });
-    }
+			}
+		});
 
-	@Override
-	public void onClick(View v) {
-		String status = editStatus.getText().toString();
-		
-		YambaClient yamba = new YambaClient("student","password");
-		try {
-			yamba.postStatus(status);
-		} catch (YambaClientException e) {
-			Log.e(TAG, "Failed to post");
-			e.printStackTrace();
-		}
-		
-		Log.d(TAG, "onClicked with status: "+status);
+		buttonTweet.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String status = editStatus.getText().toString();
+				new PostTask().execute(status);
+
+				Log.d(TAG, "onClicked with status: " + status);
+			}
+		});
 	}
 
+	private final class PostTask extends AsyncTask<String, Void, String> {
+		ProgressDialog dialog;
+		
+		// Runs on the UI thread prior to doInBackground()
+		@Override
+		protected void onPreExecute() {
+			dialog = ProgressDialog.show(StatusActivity.this, "Posting...", "Please wait");
+		}
 
+		// Executes on a non-UI thread
+		@Override
+		protected String doInBackground(String... params) {
+			final YambaClient yamba = new YambaClient("student", "password");
+			String message;
+			try {
+				yamba.postStatus(params[0]);
+				message = "Successfully posted!";
+			} catch (YambaClientException e) {
+				message = "Failed to post";
+				Log.e(TAG, message);
+				e.printStackTrace();
+			}
+			return message;
+		}
+		
+		// Executes on UI thread, after doInBackground()
+		@Override
+		protected void onPostExecute(String result) {
+			dialog.dismiss();
+			
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+		}
+	}
+	
 }
