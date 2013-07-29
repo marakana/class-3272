f58f6142e2f7ac172f68aba080711ea5c9854086
Marko Gargenta <marko@marakana.com>
2013-07-29 11:58:53 -0700

    Basic UI

--- Yamba/src/com/symantec/yamba/StatusActivity.java
+++ Yamba/src/com/symantec/yamba/StatusActivity.java
@@ -1,23 +1,33 @@
 package com.symantec.yamba;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener {
+	private Button buttonTweet;
+	private EditText editStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+        
+        buttonTweet = (Button) findViewById(R.id.button_tweet);
+        editStatus = (EditText) findViewById(R.id.edit_status);
+        
+        buttonTweet.setOnClickListener( this );
     }
 
+	@Override
+	public void onClick(View v) {
+		String status = editStatus.getText().toString();
+		Log.d("StatusActivity", "onClicked with status: "+status);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.status, menu);
-        return true;
-    }
-    
+
 }
