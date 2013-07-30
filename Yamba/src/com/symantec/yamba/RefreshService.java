579c5a353c8adbab77103290c43cd0ffc4de5b2e
Marko Gargenta <marko@marakana.com>
2013-07-30 09:32:40 -0700

    Implemented RefreshService as regular Service

--- Yamba/src/com/symantec/yamba/RefreshService.java
+++ Yamba/src/com/symantec/yamba/RefreshService.java
@@ -1,9 +1,15 @@
 package com.symantec.yamba;
 
+import java.util.List;
+
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
+
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClient.Status;
+import com.marakana.android.yamba.clientlib.YambaClientException;
 
 public class RefreshService extends Service {
 	private static final String TAG = "RefreshService";
@@ -13,13 +19,27 @@ public class RefreshService extends Service {
 		super.onCreate();
 		Log.d(TAG, "onCreated");
 	}
-	
+
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Log.d(TAG, "onStarted");
+
+		YambaClient yamba = new YambaClient("student", "password");
+		try {
+			List<Status> timeline = yamba.getTimeline(20);
+			for (Status status : timeline) {
+				Log.d(TAG,
+						String.format("%s: %s", status.getUser(),
+								status.getMessage()));
+			}
+		} catch (YambaClientException e) {
+			Log.e(TAG, "Failed to fetch timeline", e);
+			e.printStackTrace();
+		}
+
 		return super.onStartCommand(intent, flags, startId);
 	}
-	
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
