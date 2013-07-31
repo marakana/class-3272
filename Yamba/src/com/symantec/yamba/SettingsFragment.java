d3724378d8b0ddea075b6be207ac7f87ee726894
Marko Gargenta <marko@marakana.com>
2013-07-31 16:59:42 -0700

    Updated for interval changes

--- Yamba/src/com/symantec/yamba/SettingsFragment.java
+++ Yamba/src/com/symantec/yamba/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.symantec.yamba;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
@@ -9,5 +10,13 @@ public class SettingsFragment extends PreferenceFragment {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.settings);
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+
+		getActivity().sendBroadcast(
+				new Intent("com.symantec.yamba.action.UPDATE_INTERVAL"));
 	}
 }
