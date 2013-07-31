6e385ccc11ec2a941d95cef05cae520c812f18a2
Marko Gargenta <marko@marakana.com>
2013-07-31 11:25:47 -0700

    Implemented Loader

--- Yamba/src/com/symantec/yamba/TimelineFragment.java
+++ Yamba/src/com/symantec/yamba/TimelineFragment.java
@@ -1,6 +1,9 @@
 package com.symantec.yamba;
 
 import android.app.ListFragment;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.text.format.DateUtils;
@@ -14,35 +17,60 @@ public class TimelineFragment extends ListFragment {
 			StatusContract.Column.MESSAGE, StatusContract.Column.CREATED_AT };
 	private static final int[] TO = { R.id.text_user, R.id.text_message,
 			R.id.text_createdAt };
+	private static final int LOADER_ID = 42;
+	private SimpleCursorAdapter adapter;
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 
-		Cursor cursor = getActivity().getContentResolver().query(
-				StatusContract.CONTENT_URI, null, null, null, null);
-
-		SimpleCursorAdapter adapter = new SimpleCursorAdapter(getActivity(),
-				R.layout.list_item, cursor, FROM, TO, 0);
+		adapter = new SimpleCursorAdapter(getActivity(), R.layout.list_item,
+				null, FROM, TO, 0);
 		adapter.setViewBinder(VIEW_BINDER);
 
 		setListAdapter(adapter);
+
+		getLoaderManager().initLoader(LOADER_ID, null, loaderCallbacks);
 	}
-	
+
+	private final LoaderCallbacks<Cursor> loaderCallbacks = new LoaderCallbacks<Cursor>() {
+
+		@Override
+		public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+			if (id != LOADER_ID)
+				return null;
+
+			return new CursorLoader(getActivity(), StatusContract.CONTENT_URI,
+					null, null, null, null);
+		}
+
+		@Override
+		public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+			adapter.swapCursor(cursor);
+		}
+
+		@Override
+		public void onLoaderReset(Loader<Cursor> loader) {
+			adapter.swapCursor(null);
+		}
+
+	};
+
 	private static final ViewBinder VIEW_BINDER = new ViewBinder() {
 
-		@Override 
+		@Override
 		public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
-			if( view.getId() != R.id.text_createdAt )
+			if (view.getId() != R.id.text_createdAt)
 				return false;
-			
+
 			// Custom binding for timestamp
 			long timestamp = cursor.getLong(columnIndex);
-			CharSequence relTime = DateUtils.getRelativeTimeSpanString(timestamp);
-			((TextView)view).setText(relTime);
-			
+			CharSequence relTime = DateUtils
+					.getRelativeTimeSpanString(timestamp);
+			((TextView) view).setText(relTime);
+
 			return true;
 		}
-		
+
 	};
 }
